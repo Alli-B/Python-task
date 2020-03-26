280f95b8c8008c6278cf1c0021b63063976c7f2b
Alli-B <allib2089@gmail.com>
2020-03-26 23:26:24 +0100

    Add files via upload

---
+++ PY task.py
@@ -0,0 +1,7 @@
+import math 
+
+radius = input("Enter the radius of the circle you want to calculate : ")
+
+Area = math.pi * int(radius) * int(radius)
+
+print ("The Radius of the circle is : ", Area )
